aa66767598c90d6301bb9c7b92b5890751726446
Lancelot-SO <fsowah001@gmail.com>
2023-02-21 12:02:17 +0000

    add navigation bar and implement routing

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
 import React, { Component } from 'react';
+import {
+  Route, BrowserRouter, Routes,
+} from 'react-router-dom';
 import Calculator from './components/Calculator';
+import Home from './components/Home';
+import Navbar from './components/Navbar';
+import './components/Navbar.css';
+import Quote from './components/Quote';
 
 export default class App extends Component {
   constructor(props) {
@@ -9,9 +16,14 @@ export default class App extends Component {
 
   render() {
     return (
-      <div className="App">
-        <Calculator />
-      </div>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/calculator" element={<Calculator />} />
+          <Route path="/quote" element={<Quote />} />
+        </Routes>
+      </BrowserRouter>
     );
   }
 }
---
+++ src/components/Quote.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Quote.css';
+
+const Quote = () => (
+  <div className="quote">
+    <p>Mathematics consists of proving the most obvious thing in the least obvious way.</p>
+  </div>
+);
+
+export default Quote;
